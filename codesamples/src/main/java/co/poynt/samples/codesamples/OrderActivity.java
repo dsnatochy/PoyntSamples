f038e2465c82f1f2cf32246046c7687b75ca8eed
dsnatochy <dennis@poynt.co>
2017-07-17 20:33:21 -0700

    testing updateOrder

--- codesamples/src/main/java/co/poynt/samples/codesamples/OrderActivity.java
+++ codesamples/src/main/java/co/poynt/samples/codesamples/OrderActivity.java
@@ -15,22 +15,34 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import co.poynt.api.model.Business;
+import co.poynt.api.model.ClientContext;
+import co.poynt.api.model.FulfillmentStatus;
 import co.poynt.api.model.Order;
 import co.poynt.api.model.OrderAmounts;
 import co.poynt.api.model.OrderItem;
 import co.poynt.api.model.OrderItemStatus;
 import co.poynt.api.model.OrderStatus;
 import co.poynt.api.model.OrderStatuses;
+import co.poynt.api.model.Tax;
+import co.poynt.api.model.TransactionAmounts;
+import co.poynt.api.model.TransactionInstruction;
+import co.poynt.api.model.TransactionSource;
+import co.poynt.api.model.TransactionStatusSummary;
 import co.poynt.api.model.UnitOfMeasure;
 import co.poynt.os.contentproviders.orders.orders.OrdersColumns;
 import co.poynt.os.contentproviders.orders.orders.OrdersCursor;
@@ -97,17 +109,126 @@ public class OrderActivity extends Activity {
         actionBar.setDisplayHomeAsUpEnabled(true);
 
         createOrderBtn = (Button) findViewById(R.id.createOrderBtn);
+        String orderJson = "{ \"_id\": 6192, \"amounts\": { \"capturedTotals\": { \"cashbackAmount\": 0, \"orderAmount\": 2705, \"tipAmount\": 0, \"transactionAmount\": 2705 }, \"currency\": \"AED\", \"feeTotal\": 0, \"netTotal\": 2705, \"subTotal\": 2099, \"taxTotal\": 606 }, \"context\": { \"source\": \"WEB\", \"transactionInstruction\": \"EXTERNALLY_PROCESSED\" }, \"createdAt\": { \"year\": 2017, \"month\": 4, \"dayOfMonth\": 23, \"hourOfDay\": 15, \"minute\": 12, \"second\": 20 }, \"id\": \"65cde091-794e-4f69-bcb2-1b4f262cb042\", \"items\": [ { \"createdAt\": { \"year\": 2017, \"month\": 4, \"dayOfMonth\": 23, \"hourOfDay\": 15, \"minute\": 12, \"second\": 20 }, \"discount\": 0, \"fee\": 0, \"id\": 1, \"name\": \"Almond Tart\", \"productId\": \"2194f1b1-b874-4318-8686-cf81e012dcce\", \"quantity\": 1.0, \"sku\": \"082011500003\", \"status\": \"FULFILLED\", \"tax\": 118, \"taxExempted\": false, \"taxes\": [ { \"amount\": 18, \"id\": \"24da929c-8f89-4bb9-b16d-46dd4fb6627b\", \"type\": \"VAT\" }, { \"amount\": 100, \"id\": \"f87a8ed9-ecd1-4ff6-aa5d-00c86e0b2b59\", \"type\": \"state tax\" } ], \"unitOfMeasure\": \"EACH\", \"unitPrice\": 350 }, { \"createdAt\": { \"year\": 2017, \"month\": 4, \"dayOfMonth\": 23, \"hourOfDay\": 15, \"minute\": 12, \"second\": 20 }, \"discount\": 0, \"fee\": 0, \"id\": 2, \"name\": \"BBQ Chicken\", \"productId\": \"bcf1b34c-3ec8-480a-818e-1ffbd99c5048\", \"quantity\": 1.0, \"sku\": \"082011500017\", \"status\": \"FULFILLED\", \"tax\": 137, \"taxExempted\": false, \"taxes\": [ { \"amount\": 37, \"id\": \"24da929c-8f89-4bb9-b16d-46dd4fb6627b\", \"type\": \"VAT\" }, { \"amount\": 100, \"id\": \"f87a8ed9-ecd1-4ff6-aa5d-00c86e0b2b59\", \"type\": \"state tax\" } ], \"unitOfMeasure\": \"EACH\", \"unitPrice\": 749 }, { \"createdAt\": { \"year\": 2017, \"month\": 4, \"dayOfMonth\": 23, \"hourOfDay\": 15, \"minute\": 12, \"second\": 20 }, \"discount\": 0, \"fee\": 0, \"id\": 3, \"name\": \"BitterGourd\", \"productId\": \"3866ecb2-493e-48d8-b82e-46d7429f64a9\", \"quantity\": 1.0, \"sku\": \"bittergourd\", \"status\": \"FULFILLED\", \"tax\": 115, \"taxExempted\": false, \"taxes\": [ { \"amount\": 15, \"id\": \"24da929c-8f89-4bb9-b16d-46dd4fb6627b\", \"type\": \"VAT\" }, { \"amount\": 100, \"id\": \"f87a8ed9-ecd1-4ff6-aa5d-00c86e0b2b59\", \"type\": \"state tax\" } ], \"unitOfMeasure\": \"EACH\", \"unitPrice\": 300 }, { \"createdAt\": { \"year\": 2017, \"month\": 4, \"dayOfMonth\": 23, \"hourOfDay\": 15, \"minute\": 12, \"second\": 20 }, \"discount\": 0, \"fee\": 0, \"id\": 4, \"name\": \"BROWNIES\", \"productId\": \"db79b9ee-21c8-4489-9e76-3aea24e3644d\", \"quantity\": 1.0, \"sku\": \"082011500000\", \"status\": \"FULFILLED\", \"tax\": 118, \"taxExempted\": false, \"taxes\": [ { \"amount\": 18, \"id\": \"24da929c-8f89-4bb9-b16d-46dd4fb6627b\", \"type\": \"VAT\" }, { \"amount\": 100, \"id\": \"f87a8ed9-ecd1-4ff6-aa5d-00c86e0b2b59\", \"type\": \"state tax\" } ], \"unitOfMeasure\": \"EACH\", \"unitPrice\": 350 }, { \"createdAt\": { \"year\": 2017, \"month\": 4, \"dayOfMonth\": 23, \"hourOfDay\": 15, \"minute\": 12, \"second\": 20 }, \"discount\": 0, \"fee\": 0, \"id\": 5, \"name\": \"Cappuccino\", \"productId\": \"719d2bb0-5c62-478c-97c5-177de7b0fbd1\", \"quantity\": 1.0, \"sku\": \"082011500008\", \"status\": \"FULFILLED\", \"tax\": 118, \"taxExempted\": false, \"taxes\": [ { \"amount\": 18, \"id\": \"24da929c-8f89-4bb9-b16d-46dd4fb6627b\", \"type\": \"VAT\" }, { \"amount\": 100, \"id\": \"f87a8ed9-ecd1-4ff6-aa5d-00c86e0b2b59\", \"type\": \"state tax\" } ], \"unitOfMeasure\": \"EACH\", \"unitPrice\": 350 } ], \"statuses\": { \"fulfillmentStatus\": \"FULFILLED\", \"status\": \"COMPLETED\", \"transactionStatusSummary\": \"EXTERNALLY_PROCESSED\" }, \"taxExempted\": false, \"updatedAt\": { \"year\": 2017, \"month\": 4, \"dayOfMonth\": 23, \"hourOfDay\": 15, \"minute\": 12, \"second\": 20 }} ";
+        Gson gson = new Gson();
+        Type orderType = new TypeToken<Order>(){}.getType();
+        final Order order = gson.fromJson(orderJson, orderType);
+
         createOrderBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                try {
-                    orderService.createOrder(generateOrder(), UUID.randomUUID().toString(), createOrderServiceListener);
-                } catch (RemoteException e) {
-                    e.printStackTrace();
-                }
+//                try {
+//                    orderService.createOrder(generateOrder(), UUID.randomUUID().toString(), createOrderServiceListener);
+//                } catch (RemoteException e) {
+//                    e.printStackTrace();
+//                }
+                //completeOrder(order);
+                updateOrderTest();
             }
         });
         ButterKnife.bind(this);
     }
+
+
+    public void updateOrderTest(){
+        String orderJson = "{\"discounts\":[{\"amount\":-400,\"customName\":\"Order level discount\"}],\"items\":[{\"quantity\":10.0,\"id\":1,\"discounts\":[{\"amount\":50,\"customName\":\"custom discount\"}],\"unitPrice\":100,\"discount\":500,\"fee\":0,\"status\":\"FULFILLED\",\"name\":\"Small coffee\",\"sku\":\"sku12348\",\"unitOfMeasure\":\"EACH\"}],\"amounts\":{\"subTotal\":1000,\"discountTotal\":-900,\"netTotal\":100,\"currency\":\"USD\"},\"statuses\":{\"fulfillmentStatus\":\"FULFILLED\",\"status\":\"OPENED\",\"transactionStatusSummary\":\"NONE\"},\"notes\":\"will pick up at 5pm\"}";
+        Type orderType = new TypeToken<Order>(){}.getType();
+        Gson gson = new Gson();
+        Order order = gson.fromJson(orderJson, orderType);
+
+        final List<Order> orderList = new ArrayList();
+        final CountDownLatch latch = new CountDownLatch(1);
+        try {
+            orderService.createOrder(order, UUID.randomUUID().toString(), new IPoyntOrderServiceListener.Stub(){
+                @Override
+                public void orderResponse(Order order, String s, PoyntError poyntError) throws RemoteException {
+                    orderList.add(order);
+                    latch.countDown();
+                }
+            });
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        order = orderList.get(0);
+//        order.getStatuses().setTransactionStatusSummary(TransactionStatusSummary.EXTERNALLY_PROCESSED);
+//        order.getStatuses().setStatus(OrderStatus.COMPLETED);
+
+        try {
+            orderService.updateOrder(order.getId().toString(), order, UUID.randomUUID().toString(),
+                    new IPoyntOrderServiceListener.Stub() {
+                        @Override
+                        public void orderResponse(Order order, String s, PoyntError poyntError) throws RemoteException {
+                            System.out.println(order);
+                        }
+
+
+                    });
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void completeOrder(Order order) {
+        Log.i("completeOrder", order.getId().toString());
+
+        long subtotal = 0L;
+        long taxTotal = 0L;
+        for (int i = 0; i < order.getItems().size(); i++) {
+            OrderItem item = order.getItems().get(i);
+            item.setStatus(OrderItemStatus.FULFILLED);
+            subtotal += item.getUnitPrice() * item.getQuantity();
+            taxTotal += item.getTax();
+        }
+        OrderStatuses orderStatuses = new OrderStatuses();
+        orderStatuses.setStatus(OrderStatus.COMPLETED);
+        orderStatuses.setFulfillmentStatus(FulfillmentStatus.FULFILLED);
+        orderStatuses.setTransactionStatusSummary(TransactionStatusSummary.EXTERNALLY_PROCESSED);
+        order.setStatuses(orderStatuses);
+        // start
+        if (order.getContext() != null) {
+            order.setContext(new ClientContext());
+        }
+        order.getContext().setSource(TransactionSource.WEB);
+        order.getContext().setTransactionInstruction(TransactionInstruction.EXTERNALLY_PROCESSED);
+        // end
+
+
+        TransactionAmounts capturedAmount = new TransactionAmounts();
+        capturedAmount.setCashbackAmount(0L);
+        capturedAmount.setOrderAmount(order.getAmounts().getNetTotal());
+        capturedAmount.setTipAmount(0L);
+        capturedAmount.setTransactionAmount(order.getAmounts().getNetTotal());
+        //order.getAmounts().setCapturedTotals(capturedAmount);
+        OrderAmounts orderAmounts = new OrderAmounts();
+       // orderAmounts.setCapturedTotals(capturedAmount);
+        orderAmounts.setNetTotal(order.getAmounts().getNetTotal());
+        orderAmounts.setTaxTotal(taxTotal);
+        orderAmounts.setFeeTotal(order.getAmounts().getFeeTotal());
+        orderAmounts.setSubTotal(subtotal);
+        orderAmounts.setCurrency(order.getAmounts().getCurrency());
+        order.setAmounts(orderAmounts);
+
+        Gson gson = new Gson();
+        Type orderType = new TypeToken<Order>(){}.getType();
+        System.out.println(gson.toJson(order, orderType));
+
+        order.setId(UUID.randomUUID());
+        try {
+            orderService.completeOrder(order.getId().toString(), order, UUID.randomUUID().toString(), orderServiceListener);
+
+            //orderService.createOrder(order, UUID.randomUUID().toString(), orderServiceListener);
+//            DrawerActivity.orderService.captureOrder(order.getId().toString(), order, UUID.randomUUID().toString(), completeOrderListener);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     protected void onResume() {
         super.onResume();
